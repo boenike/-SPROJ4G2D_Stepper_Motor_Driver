dd887d41a040624ff7ce5be2a02d13ef23550d40
Bence Tóth <bencike04201@gmail.com>
2025-04-25 12:25:50 +0200

    TB for next flag enabler
    TB for next flag enabler

--- Half_Stepper/Half_Stepping_TB.vhd
+++ Half_Stepper/Half_Stepping_TB.vhd
@@ -10,37 +10,40 @@ end Half_Stepping_TB;
 architecture Behavioral of Half_Stepping_TB is
     Component Half_Stepping_Top is
         Port ( DIR , GCK , EN : in STD_LOGIC;
-           OUTPUT : out STD_LOGIC_VECTOR (7 downto 0)
+               NXT_FLAG : out STD_LOGIC;
+               OUTPUT : out STD_LOGIC_VECTOR (7 downto 0)
         );
     end Component;
 
     constant ClockFrequency : integer := 100e6; -- 100 MHz
     constant ClockPeriod    : time    := 1000 ms / ClockFrequency;
-    constant waitPeriod     : time    := 1 ms;
+    constant waitPeriod     : time    := 1.2 ms;
 
-    Signal CLCK: STD_LOGIC := '1' ;
+    Signal GCK: STD_LOGIC := '1' ;
     Signal DIR: STD_LOGIC := '1' ;  -- Forward
     Signal EN: STD_LOGIC := '1' ;   -- Output Enabled
     Signal OUTPUT : STD_LOGIC_VECTOR (7 downto 0);
+    Signal NXT_FLAG: STD_LOGIC ;
 
     begin
 
         uut: Half_Stepping_Top Port map
-        (DIR=>DIR, EN=>EN, GCK=>CLCK, OUTPUT=>OUTPUT);
+        (DIR=>DIR, EN=>EN, GCK=>GCK, OUTPUT=>OUTPUT, NXT_FLAG=>NXT_FLAG);
     
-        CLOCK: process (CLCK) is
+        CLOCK: process (GCK) is
             begin
                 -- Process for generating the clock
-                CLCK <= not CLCK after ClockPeriod / 2;
+                GCK <= not GCK after ClockPeriod / 2;
         end process CLOCK;
         
         Testbench: process is
             begin
                 wait for 8*waitPeriod;
                     DIR <= '0';
-                wait for 12*waitPeriod;
+                wait for 4*waitPeriod;
                     DIR <= '1';
                     EN <= '0';
                 wait for 8*waitPeriod;
+                    EN <= '1';
         end process Testbench;
 end Behavioral;
